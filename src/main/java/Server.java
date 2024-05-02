89619d940e0a8cee45c698727ba426a684a9a5bc
Vitaliy Murzin <murzinvit@yandex.ru>
2024-05-02 19:04:40 +0500

    add flow to try

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,12 +7,12 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(8100); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-            System.out.println("Server started on port 8100");
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
+        System.out.println("Server started on port 8100");
+        try(ServerSocket serverSocket = new ServerSocket(8100);
+            Socket clientSocket = serverSocket.accept();
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
             System.out.println("New connection accepted");
             final String name = in.readLine();
             out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
