3f5deed6f1b6474f9d161ab4e8ddda3abb94b4f5
Vitaliy Murzin <murzinvit@yandex.ru>
2024-05-03 15:31:53 +0500

    add quit to Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,22 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         String host = "localhost";
         int port = 8100;
-        try (Socket clientSocket = new Socket(host, port);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("quit\n" + "Host: localhost\n\n\n");
-            String resp = in.readLine();
-            if(resp == null) {
-                return;
+        while (true) {
+            try (Socket clientSocket = new Socket(host, port);
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                System.out.println("Enter your message(Input 'quit' to exit):");
+                String message = sc.nextLine();
+                out.println(message + "\n" + "Host: localhost\n\n\n");
+                String resp = in.readLine();
+                if (resp == null) {
+                    return;
+                }
+                System.out.println(resp);
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-            System.out.println(resp);
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
     }
 }
